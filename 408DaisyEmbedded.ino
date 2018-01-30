ac0eb4a2e5354ded508a18aea233a9c863a0b46e
Jesse Pai <jesse.pai@gmail.com>
2018-01-30 18:46:00 -0500

    Added some basic code

--- 408DaisyEmbedded.ino
+++ 408DaisyEmbedded.ino
@@ -1,8 +1,17 @@
 #include <Daisy.h>
 
+Daisy daisy();
+
 void setup() {
   // put your setup code here, to run once:
-
+  for (int i = 0; i < 400; i++) {
+    daisy.forward(100)
+    delay(1);
+  }
+  for (int i = 0; i < 400; i++) {
+    daisy.backward(100);
+    delay(1);
+  }
 }
 
 void loop() {
