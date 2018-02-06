b64406b4f23bf779794a1501d4d04c2d5f75d9ea
Jesse Pai <jesse.pai@gmail.com>
2018-02-05 23:37:04 -0500

    Added default initialization to setup() function in base ino file (not really necessary).

--- 408DaisyEmbedded.ino
+++ 408DaisyEmbedded.ino
@@ -3,12 +3,12 @@
 Daisy daisy;
 
 void setup() {
-
+  // put your setup code here, to run once:
+  daisy = Daisy();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-    // put your setup code here, to run once:
   for (int i = 0; i < 400; i++) {
     daisy.forward(100);
     delay(1);
