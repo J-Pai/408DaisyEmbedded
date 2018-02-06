71a497fe302ceb788fd8a0d6eeef900535ab12c2
j-pai <jesse.pai@gmail.com>
2018-02-06 14:20:00 -0500

    Test commit

--- 408DaisyEmbedded.ino
+++ 408DaisyEmbedded.ino
@@ -1,16 +1,19 @@
 #include <Daisy.h>
 
+#define THRESHOLD 5
+
 Daisy daisy;
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.println("Setting up Daisy");
   daisy = Daisy(3,5);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-    if(daisy.leftPingIN() <= 5 || daisy.rightPingIN() <= 5){
+    if(daisy.leftPingIN() <= THRESHOLD || daisy.rightPingIN() <= THRESHOLD){
       daisy.halt();
     } else {
       daisy.forward(100);
