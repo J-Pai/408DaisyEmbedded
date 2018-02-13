87c9fe72d1ade986f75fa29326a54c8a9682ecdd
school <school@example.com>
2018-02-13 12:54:12 -0500

    Test drive

--- 408DaisyEmbedded.ino
+++ 408DaisyEmbedded.ino
@@ -15,14 +15,22 @@ void setup() {
     PRINTLN("Daisy has been initialized!")
 }
 
-void loop() {
-    // put your main code here, to run repeatedly:
-    unsigned long dist = daisy.leftPingIN();
-    PRINTLN("%lu %lu", dist, THRESHOLD)
-    if(dist <= THRESHOLD) {
-      //PRINTLN("HALTING")
-      daisy.halt();
-    } else {
-      daisy.forward(100);
+
+
+    
+void loop(){
+
+    unsigned long distR = daisy.rightPingIN();
+    unsigned long distL = daisy.leftPingIN();
+    
+    if(distR <= 5){
+      daisy.turn(CCW, 100);
+    }
+    if(distL <= 5){
+      daisy.turn(CW, 100);
+    }
+    else{
+       daisy.forward(100);
     }
 }
+
