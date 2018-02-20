afd36b6869f73ebff22e93545f46284b61afdf53
Teddy Mengistu <teddy@wireless-10-104-44-62.umd.edu>
2018-02-20 13:13:32 -0500

    updated code for arduino controls including middle ping

--- 408DaisyEmbedded.ino
+++ 408DaisyEmbedded.ino
@@ -1,28 +1,36 @@
 #include <Daisy.h>
 
-/** 
- * This is being used to compared against an
- * unsigned long. Make sure to add an L behind
- * the constant.
- */
 #define THRESHOLD 5L
 
 Daisy daisy;
 
 void setup() {
-    // put your setup code here, to run once:
-    daisy = Daisy(3,5,11);
-    PRINTLN("Daisy has been initialized!")
+  // put your setup code here, to run once:
+  daisy = Daisy(3,5,11);
+ // PRINTLN("Daisy initialized!");
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
-    unsigned long dist = daisy.leftPingIN();
-    PRINTLN("%lu %lu", dist, THRESHOLD)
-    if(dist <= THRESHOLD) {
-      //PRINTLN("HALTING")
-      daisy.halt();
-    } else {
-      daisy.forward(100);
-    }
+
+  unsigned long leftPing = daisy.leftPingIN();
+  unsigned long rightPing = daisy.rightPingIN();
+  unsigned long middlePing = daisy.middlePingIN();
+ 
+  if(middlePing <= THRESHOLD){ // if center ping see obstacle and others dont then Stop
+    daisy.halt();
+  }
+  else if(leftPing <= THRESHOLD && rightPing <= THRESHOLD){ // if both sides have obstacles in front of them turn until 
+    daisy.turn(CW,100);
+  }
+  else if(leftPing <= THRESHOLD && rightPing >= THRESHOLD){ // avoid obstacle on the left and turn 
+    daisy.turn(CW,100);
+  }
+  else if(rightPing <= THRESHOLD && leftPing >= THRESHOLD){ // avoid obstacle on the right and turn 
+    daisy.turn(CCW,100);
+  }
+  else{
+    daisy.forward(100);
+  }
+  
+    
 }
