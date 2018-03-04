1e741066761433c8cbe43d2a2a7d4a658a8f782d
j-pai <jesse.pai@gmail.com>
2018-03-04 15:12:05 -0500

    Back to fastest rate. NOTE: do not use micro USB port on Jetson. Causes the IDE to freeze.

--- Daisy/Daisy.h
+++ Daisy/Daisy.h
@@ -1,7 +1,7 @@
 #ifndef Daisey_h
 #define Daisey_h
 
-#define BAUD_RATE 9600 //28800
+#define BAUD_RATE 28800
 // Following three lines enables DEBUG and PRINT macro operations. Make sure
 // to include Daisy_Print.h header file.
 #define ENABLE_DEBUG 0 // Set to 1 to enable debugging print statements
